af8668dc893934c42e61273b4660ba9de9d761d6
na0ki_mac <wehallo53.en@gmail.com>
2022-05-24 08:14:32 +0900

    ../result/first_SVM_nolabelencorde/

--- med_insur/source/first.py
+++ med_insur/source/first.py
@@ -9,6 +9,9 @@ from sklearn.model_selection import KFold, GroupKFold
 
 from sklearn.metrics import mean_squared_error
 import pickle
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+
 
 
 import json
@@ -54,6 +57,19 @@ def pred_logi(train_X,train_Y,test_X):
     train_rmse=np.sqrt(mean_squared_error(train_Y, model.predict_proba(train_X)[:,1]))
     return pred,train_rmse,model
 
+def pred_svm(train_X,train_Y,test_X): 
+    #LinearSVMのインスタンスを作成
+    Linsvc = svm.LinearSVC(random_state=0, max_iter=3000)
+    
+    #LinearSVMでデータを学習
+    Linsvc.fit(train_X,train_Y)
+    pred = Linsvc.predict(test_X)
+    train_results={}
+    train_results["co_matrix"]=str(confusion_matrix(y_true = train_Y, y_pred = Linsvc.predict(train_X)))
+    train_results["acc"]=Linsvc.score(train_X,train_Y)
+
+    return pred,train_results,Linsvc
+
 def pred_lightgpm(train_X,train_Y,test_X):
 #https://rightcode.co.jp/blog/information-technology/lightgbm-useful-for-kaggler
     # LightGBM用のデータセットに入れる
@@ -85,15 +101,21 @@ def first_read_plot_pred(dropparms,out_path):
     train_X,train_Y,test_X=make_XY(train,test)
     out_json["OBJPRM"]=OBJPRM
     
-    if out_path=="../result/first_Logi_nolabelencorde/":
+    if out_path=="../result/first_Logi_nolabelencorde/" or out_path=="../result/first_SVM_nolabelencorde/":
         train_X=train_X.drop(dropparms, axis=1) # 目的変数を除いたデータ
         test_X=test_X.drop(dropparms, axis=1) # 目的変数を除いたデータ
         out_json["dropoarms"]=dropparms
 
-    pred,train_rmse,model=pred_logi(train_X,train_Y,test_X)
-    out_json["model_name"]="logosticReg"
-    out_json["train_rmse"]=train_rmse
 
+    if out_path=="../result/first_Logi_nolabelencorde/":
+        pred,train_rmse,model=pred_logi(train_X,train_Y,test_X)
+        out_json["model_name"]="logosticReg"
+        out_json["train_rmse"]=train_rmse
+    elif out_path=="../result/first_SVM_nolabelencorde/":
+        pred,train_results,model=pred_svm(train_X,train_Y,test_X)
+        out_json["model_name"]="logosticReg"
+        out_json["train_results"]=train_results
+    
     out_json["out_path"]=out_path
     if not os.path.isdir(out_path):
         try:
@@ -114,4 +136,8 @@ def first_read_plot_pred(dropparms,out_path):
 if __name__=="__main__":
     dropparms=["sex","smoker","region"]
     out_path="../result/first_Logi_nolabelencorde/"
+    first_read_plot_pred(dropparms,out_path)
+
+    dropparms=["sex","smoker","region"]
+    out_path="../result/first_SVM_nolabelencorde/"
     first_read_plot_pred(dropparms,out_path)
