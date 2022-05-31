a0bd77c316b13e3735434c867d5da37f09b3f5d2
na0ki_mac <wehallo53.en@gmail.com>
2022-05-31 16:16:14 +0900

    lightgbm ok

---
+++ rensyu_gbm.py
@@ -0,0 +1,10 @@
+# 必要なライブラリのインストール
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import lightgbm as lgb
+import time
+ 
+from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
